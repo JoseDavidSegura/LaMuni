f9df582fdec2ee411494cd931ba44d3be442f451
JoseDavid <33429026+JoseDavidSegura@users.noreply.github.com>
2018-01-20 00:11:29 -0500

    Update Validaciones.cs

--- LaMuni/Helpers/Validaciones.cs
+++ LaMuni/Helpers/Validaciones.cs
@@ -1,10 +1,26 @@
-﻿using System;
+using System.Text.RegularExpressions;
+
+
 namespace LaMuni.Helpers
 {
     public class Validaciones
     {
-        public Validaciones()
+
+
+
+
+
+
+        //Validar formato de email
+        public static bool IsValidEmail(string strMailAddress)
         {
+            
+            // Return true if strIn is in valid e-mail format.
+            return Regex.IsMatch(strMailAddress, @"^(?("")("".+?""@)|(([0-9a-zA-Z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-zA-Z])@))" + @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,6}))$");
         }
+
+
+       
+
     }
 }
