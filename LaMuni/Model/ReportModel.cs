f5a7d866a7231fb53746919a7a8d8b1758c12046
JoseDavid <33429026+JoseDavidSegura@users.noreply.github.com>
2018-01-19 23:58:02 -0500

    Update ReportModel.cs

--- LaMuni/Model/ReportModel.cs
+++ LaMuni/Model/ReportModel.cs
@@ -1,10 +1,28 @@
-﻿using System;
+using System;
+using Plugin.Media.Abstractions;
+using Realms;
+
 namespace LaMuni.Model
 {
-    public class ReportModel
+    public class ReportModel : RealmObject
     {
-        public ReportModel()
-        {
-        }
+
+        #region Atributos
+
+        public string reportId { get; set; }
+        public string reporter { get; set; }
+        public string reportDate { get; set; }
+        public string reportType { get; set; }
+        public string reportStatus { get; set; }
+        public string detail { get; set; }
+        public double latKey { get; set; }
+        public double lonKey { get; set; }
+        public string updateLog { get; set; }
+        public string responseSummary { get; set; }
+        public string responseDate { get; set; }
+        public string responseReporter { get; set; }
+        public byte[] photo { get; set; }
+
+        #endregion
     }
 }
