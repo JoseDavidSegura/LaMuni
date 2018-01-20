760755ca5a42be67e05eaf05e0f5d1e08dae23c6
JoseDavid <33429026+JoseDavidSegura@users.noreply.github.com>
2018-01-20 00:11:01 -0500

    Update UtilDB.cs

--- LaMuni/Helpers/UtilDB.cs
+++ LaMuni/Helpers/UtilDB.cs
@@ -1,10 +1,30 @@
-﻿using System;
 namespace LaMuni.Helpers
 {
+
+    using System;
+    using Realms;
+
     public class UtilDB
     {
-        public UtilDB()
+
+        public static Realm GetInstanceRealm()
         {
+
+            var config = new RealmConfiguration()
+            {
+                SchemaVersion = 2,
+                MigrationCallback = (migration, oldSchemaVersion) =>
+                {
+
+                }
+            };
+
+            var _realm = Realm.GetInstance(config);
+
+            return _realm;
+
         }
+
     }
 }
+
