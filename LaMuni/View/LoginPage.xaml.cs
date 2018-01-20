011c858890152820ae8d1166293ac17398fe547f
JoseDavid <33429026+JoseDavidSegura@users.noreply.github.com>
2018-01-20 00:16:05 -0500

    Update LoginPage.xaml.cs

--- LaMuni/View/LoginPage.xaml.cs
+++ LaMuni/View/LoginPage.xaml.cs
@@ -1,15 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
-
+using System.Linq;
+using LaMuni.Model;
+using LaMuni.ViewModel;
+using Realms;
 using Xamarin.Forms;
 
 namespace LaMuni.View
 {
     public partial class LoginPage : ContentPage
     {
+
+        private Realm _realm;
+
+        private InicioModel _inicioParam { get; set; }
+
+
+        public InicioModel InicioParam
+        {
+            get
+            {
+                return _inicioParam;
+            }
+            set
+            {
+                _inicioParam = value;
+                OnPropertyChanged("InicioParam");
+            }
+        }
+
+
         public LoginPage()
         {
             InitializeComponent();
+
+            BindingContext = new LoginViewModel();
+        }
+
+        void Handle_Toggled(object sender, Xamarin.Forms.ToggledEventArgs e)
+        {
+
+            _realm = Helpers.UtilDB.GetInstanceRealm();
+
+            InicioParam = new InicioModel();
+
+            if (RemSwitch.IsToggled)
+            {
+                _realm.Write(() =>
+                {
+                    InicioParam.RecuerdaUser = true;
+
+                    _realm.Add<InicioModel>(InicioParam, true);
+                });
+            }
+            else
+            {
+                _realm.Write(() =>
+               {
+                    InicioParam.RecuerdaUser = false;
+
+                    _realm.Add<InicioModel>(InicioParam, true);
+
+                    txtEmail.Text = "";
+
+                    txtPass.Text = "";
+               });
+            }
+
         }
     }
 }
