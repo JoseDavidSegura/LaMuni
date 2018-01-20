31b06c3e3cfaccac8d2d6cdff1b7fd8ef9b3c55a
JoseDavid <33429026+JoseDavidSegura@users.noreply.github.com>
2018-01-20 00:05:04 -0500

    Update UsersModel.cs

--- LaMuni/Model/UsersModel.cs
+++ LaMuni/Model/UsersModel.cs
@@ -1,10 +1,30 @@
-﻿using System;
 namespace LaMuni.Model
 {
-    public class UsersModel
+
+    using System;
+    using Realms;
+
+    public class UsersModel : RealmObject
     {
-        public UsersModel()
-        {
-        }
+
+        #region Atributos
+
+        [PrimaryKey]
+        public string userId { get; set; }
+        public string userNick { get; set; }
+        public string name { get; set; }
+        public string firstLastName { get; set; }
+        public string secondLastName { get; set; }
+        public string email { get; set; }
+        public string pass { get; set; }
+        public string phone { get; set; }
+        public string userType { get; set; }
+        public string token { get; set; }
+        public byte[] userPhoto { get; set; }
+
+        #endregion
+
+
     }
 }
+
